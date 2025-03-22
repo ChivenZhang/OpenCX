b8d713650a71865df854e1542e808943c06b5436
ChivenZhang <1759774248@qq.com>
2025-03-22 18:57:23 +0800

    update

--- Ocxgen/main.cpp
+++ Ocxgen/main.cpp
@@ -15,13 +15,18 @@
 #include <OpenARGS/OpenARGS.h>
 #include "Klass.h"
 
-using args_t = String;
+struct class_t
+{
+	String Name;
+};
 
 struct field_t
 {
 	String Name;
 	String Type;
 };
+
+using args_t = String;
 
 struct method_t
 {
@@ -30,66 +35,20 @@ struct method_t
 	List<args_t> Args;
 };
 
-struct class_t
-{
-	String Name;
-};
-
 struct clang_t
 {
 	class_t Class;
 	List<class_t> Bases;
-	List<field_t> Fields;
-	List<method_t> Methods;
-	List<field_t> SFields;
-	List<method_t> SMethods;
+	List<field_t> Fields, SFields;
+	List<method_t> Methods, SMethods;
 };
 
-std::string READ_FILE(std::string path)
-{
-	std::ifstream file_reader(path);
-	if (file_reader.is_open())
-	{
-		std::stringstream buffer;
-		buffer << file_reader.rdbuf();
-		file_reader.close();
-		return buffer.str();
-	}
-	PRINT("Failed to open file!" );
-	return std::string();
-}
-
-bool WRITE_FILE(std::string path, std::string content)
-{
-	std::ofstream file_writer(path, std::ios::app);
-	if (file_writer.is_open())
-	{
-		file_writer << content;
-		file_writer.close();
-		return true;
-	}
-	PRINT("Failed to open file!" );
-	return false;
-}
-
-std::string STRING_REPLACE(std::string const& string, std::string const& target, std::string const& replacement)
-{
-	std::string result;
-	size_t i = 0;
-	for (; i < string.length(); )
-	{
-		auto k = string.find(target, i);
-		if (k == std::string::npos) break;
-		result += string.substr(i, k - i) + replacement;
-		i = k + target.length();
-	}
-	if (i < string.length()) result += string.substr(i);
-	return result;
-}
-
-int ANALYSE_METADATA(String path, int argc, char** argv);
-int OUTPUT_METADATA(String path, clang_t const& meta);
-CXChildVisitResult TRAVERSE_METADATA(CXCursor current_cursor, CXCursor parent, CXClientData client_data);
+String CX_READ_FILE(String path);
+bool CX_WRITE_FILE(String path, String content);
+String CX_REPLACE_STR(String const& string, String const& target, String const& replacement);
+int CX_ANALYSE_METADATA(String path, int argc, char** argv);
+int CX_OUTPUT_METADATA(String path, clang_t const& meta);
+CXChildVisitResult CX_TRAVERSE_METADATA(CXCursor current_cursor, CXCursor parent, CXClientData client_data);
 
 int main(int argc, char** argv)
 {
@@ -105,10 +64,52 @@ int main(int argc, char** argv)
 		PRINT("file does not exist:", file);
 		return -1;
 	}
-	return ANALYSE_METADATA(file, argc, argv);
+	return CX_ANALYSE_METADATA(file, argc-2, argv+2);
 }
 
-int ANALYSE_METADATA(String file, int argc, char** argv)
+String CX_READ_FILE(String path)
+{
+	std::ifstream file_reader(path);
+	if (file_reader.is_open())
+	{
+		std::stringstream buffer;
+		buffer << file_reader.rdbuf();
+		file_reader.close();
+		return buffer.str();
+	}
+	PRINT("Failed to open file!" );
+	return String();
+}
+
+bool CX_WRITE_FILE(String path, String content)
+{
+	std::ofstream file_writer(path, std::ios::app);
+	if (file_writer.is_open())
+	{
+		file_writer << content;
+		file_writer.close();
+		return true;
+	}
+	PRINT("Failed to open file!" );
+	return false;
+}
+
+String CX_REPLACE_STR(String const& string, String const& target, String const& replacement)
+{
+	String result;
+	size_t i = 0;
+	for (; i < string.length(); )
+	{
+		auto k = string.find(target, i);
+		if (k == String::npos) break;
+		result += string.substr(i, k - i) + replacement;
+		i = k + target.length();
+	}
+	if (i < string.length()) result += string.substr(i);
+	return result;
+}
+
+int CX_ANALYSE_METADATA(String file, int argc, char** argv)
 {
 	auto index = clang_createIndex(0, 0); //Create index
 	auto unit = clang_parseTranslationUnit(
@@ -143,14 +144,14 @@ int ANALYSE_METADATA(String file, int argc, char** argv)
 	}
 	clang_t meta;
 	auto cursor = clang_getTranslationUnitCursor(unit);
-	clang_visitChildren( cursor, TRAVERSE_METADATA, &meta);
+	clang_visitChildren( cursor, CX_TRAVERSE_METADATA, &meta);
 	clang_disposeTranslationUnit(unit);
 	clang_disposeIndex(index);
 	PRINT("succeed", file);
 	return 0;
 }
 
-int OUTPUT_METADATA(String file, clang_t const& meta)
+int CX_OUTPUT_METADATA(String file, clang_t const& meta)
 {
 	auto& bases = meta.Bases;
 	auto& fields = meta.Fields, &sFields = meta.SFields;
@@ -169,82 +170,82 @@ int OUTPUT_METADATA(String file, clang_t const& meta)
 	for (auto& e : bases)
 	{
 		String s(TEMPLATE_BASE);
-		s = STRING_REPLACE(s, META_NAME, e.Name);
+		s = CX_REPLACE_STR(s, META_NAME, e.Name);
 		metaBase += s;
 	}
 
 	for (auto& e : fields)
 	{
 		String s(TEMPLATE_FIELD);
-		s = STRING_REPLACE(s, META_NAME, e.Name);
-		s = STRING_REPLACE(s, META_TYPE, e.Type);
+		s = CX_REPLACE_STR(s, META_NAME, e.Name);
+		s = CX_REPLACE_STR(s, META_TYPE, e.Type);
 		metaField += s;
 	}
 
 	for (auto& e : methods)
 	{
 		String s(TEMPLATE_METHOD);
-		s = STRING_REPLACE(s, META_NAME, e.Name);
-		s = STRING_REPLACE(s, META_RETURN, e.Type);
+		s = CX_REPLACE_STR(s, META_NAME, e.Name);
+		s = CX_REPLACE_STR(s, META_RETURN, e.Type);
 		String _t;
 		for(auto& a : e.Args) _t += (_t.empty()?"":",") + a;
-		s = STRING_REPLACE(s, META_TYPE, _t);
+		s = CX_REPLACE_STR(s, META_TYPE, _t);
 		String t;
 		for(auto& a : e.Args) t += "," + a;
-		s = STRING_REPLACE(s, META_ARGS_TYPE, t);
+		s = CX_REPLACE_STR(s, META_ARGS_TYPE, t);
 		String tt;
 		for(size_t i=0; i<e.Args.size(); ++i) tt += "," + String("auto") + " " + "_" + std::to_string(1+i);
-		s = STRING_REPLACE(s, META_ARGS_CALL, tt);
+		s = CX_REPLACE_STR(s, META_ARGS_CALL, tt);
 		String ttt;
 		for(size_t i=0; i<e.Args.size(); ++i) ttt += String(ttt.empty()?"":",") + "_" + std::to_string(1+i);
-		s = STRING_REPLACE(s, META_ARGS_PASS, ttt);
+		s = CX_REPLACE_STR(s, META_ARGS_PASS, ttt);
 		metaMethod += s;
 	}
 
 	for (auto& e : sFields)
 	{
 		String s(TEMPLATE_SFIELD);
-		s = STRING_REPLACE(s, META_NAME, e.Name);
-		s = STRING_REPLACE(s, META_TYPE, e.Type);
+		s = CX_REPLACE_STR(s, META_NAME, e.Name);
+		s = CX_REPLACE_STR(s, META_TYPE, e.Type);
 		metaSField += s;
 	}
 
 	for (auto& e : sMethods)
 	{
 		String s(TEMPLATE_SMETHOD);
-		s = STRING_REPLACE(s, META_NAME, e.Name);
-		s = STRING_REPLACE(s, META_RETURN, e.Type);
+		s = CX_REPLACE_STR(s, META_NAME, e.Name);
+		s = CX_REPLACE_STR(s, META_RETURN, e.Type);
 		String _t;
 		for(auto& a : e.Args) _t += (_t.empty()?"":",") + a;
-		s = STRING_REPLACE(s, META_TYPE, _t);
+		s = CX_REPLACE_STR(s, META_TYPE, _t);
 		String t;
 		for(auto& a : e.Args) t += "," + a;
-		s = STRING_REPLACE(s, META_ARGS_TYPE, t);
+		s = CX_REPLACE_STR(s, META_ARGS_TYPE, t);
 		String tt;
 		for(size_t i=0; i<e.Args.size(); ++i) tt += String(tt.empty()?"":",") + "auto" + " " + "_" + std::to_string(1+i);
-		s = STRING_REPLACE(s, META_ARGS_CALL, tt);
+		s = CX_REPLACE_STR(s, META_ARGS_CALL, tt);
 		String ttt;
 		for(size_t i=0; i<e.Args.size(); ++i) ttt += String(ttt.empty()?"":",") + "_" + std::to_string(1+i);
-		s = STRING_REPLACE(s, META_ARGS_PASS, ttt);
+		s = CX_REPLACE_STR(s, META_ARGS_PASS, ttt);
 		metaSMethod += s;
 	}
 
-	output = STRING_REPLACE(output, META_FILE, metaFile);
-	output = STRING_REPLACE(output, META_CLASS, metaClass);
-	output = STRING_REPLACE(output, META_BASE, metaBase);
-	output = STRING_REPLACE(output, META_FIELD, metaField);
-	output = STRING_REPLACE(output, META_METHOD, metaMethod);
-	output = STRING_REPLACE(output, META_SFIELD, metaSField);
-	output = STRING_REPLACE(output, META_SMETHOD, metaSMethod);
+	output = CX_REPLACE_STR(output, META_FILE, metaFile);
+	output = CX_REPLACE_STR(output, META_CLASS, metaClass);
+	output = CX_REPLACE_STR(output, META_BASE, metaBase);
+	output = CX_REPLACE_STR(output, META_FIELD, metaField);
+	output = CX_REPLACE_STR(output, META_METHOD, metaMethod);
+	output = CX_REPLACE_STR(output, META_SFIELD, metaSField);
+	output = CX_REPLACE_STR(output, META_SMETHOD, metaSMethod);
 
 	auto file2 = folder + "/" + baseName + ".meta.h";
-	WRITE_FILE(file2, output);
+	CX_WRITE_FILE(file2, output);
 
 	PRINT("output",  meta.Class.Name, "to", file2);
 	return 0;
 }
 
-CXChildVisitResult TRAVERSE_METADATA(CXCursor current_cursor, CXCursor parent, CXClientData client_data)
+CXChildVisitResult CX_TRAVERSE_METADATA(CXCursor current_cursor, CXCursor parent, CXClientData client_data)
 {
 	auto& meta = *(clang_t*)client_data;
 
