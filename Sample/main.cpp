716e6211fe406decf40c1c552be4b2ba989f716b
ChivenZhang <1759774248@qq.com>
2025-03-26 21:06:20 +0800

    update

--- Sample/main.cpp
+++ Sample/main.cpp
@@ -1,5 +1,4 @@
 #include <OpenCX/Class.h>
-#include "MyObject.h"
 #include "test.meta.h"
 
 int main()
@@ -8,11 +7,15 @@ int main()
 
     PRINT(Class::Get<ObjectT<MyObject>>()->getName());
 
+    // Access field
+
     Class::Set<MyObject, String>("Name", obj.get(), "你好");
     Class::SetStatic<MyObject, String>("SName", "世界");
 
     PRINT("get Name:", *Class::Get<MyObject, String>("Name", obj.get()));
     PRINT("get SName:", *Class::GetStatic<MyObject, String>("SName"));
+
+    // Access method
 
     Class::Call<MyObject, void>("Foo", obj.get());
     Class::Call<MyObject, void, String>("Foo", obj.get(), "Hello");
@@ -22,9 +25,13 @@ int main()
     Class::CallStatic<MyObject, void, String>("SFoo", "World");
     Class::CallStatic<MyObject, void, String, float>("SFoo", "World", 456.0f);
 
+    // Access inner class
+
     MyObject::InnerObject inObj;
     PRINT("get InnerObject::SubName:", *Class::Get<MyObject::InnerObject, int>("SubName", &inObj));
     PRINT("get InnerObject::SSubName:", *Class::GetStatic<MyObject::InnerObject, const int>("SSubName"));
+
+    // Access subclass
 
     MySubObject subObj;
     Class::Call<MyObject, void>("VFoo", &subObj);
